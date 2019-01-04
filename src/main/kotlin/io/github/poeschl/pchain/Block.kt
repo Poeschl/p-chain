8c7ec1bdd35384ea0c7cdbe73d8ee19e76363cac
Markus Pöschl <Poeschl@users.noreply.github.com>
2019-01-04 21:37:00 +0100

    basic chain

--- src/main/kotlin/io/github/poeschl/pchain/Block.kt
+++ src/main/kotlin/io/github/poeschl/pchain/Block.kt
@@ -6,16 +6,30 @@ import java.time.ZoneOffset
 data class Block(
     val previousHash: String, val data: String
 ) {
-    private val timestamp: Long = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)
+    internal var hash: String
 
-    internal val hash = calcHash()
+    private val timestamp: Long = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)
+    private var nonce: Int = 0
+
+    init {
+        hash = calcHash()
+    }
 
     internal fun calcHash(): String {
-        return (previousHash + timestamp.toString() + data).asSha256Hex()
+        return (previousHash + timestamp.toString() + data + nonce).asSha256Hex()
+    }
+
+    internal fun mineBlock(difficulty: Int) {
+        val target = "".padStart(difficulty, '0')
+        while (hash.substring(0, difficulty) != target) {
+            nonce++
+            hash = calcHash()
+        }
+        println("Block Mined: $hash")
     }
 
     override fun toString(): String {
-        return "Block(previousHash='$previousHash', data='$data', timestamp=$timestamp, hash='$hash')"
+        return "Block(previousHash='$previousHash', data='$data', hash='$hash', timestamp=$timestamp, nonce=$nonce)"
     }
 
 
--- src/main/kotlin/io/github/poeschl/pchain/PChain.kt
+++ src/main/kotlin/io/github/poeschl/pchain/PChain.kt
@@ -2,37 +2,62 @@ package io.github.poeschl.pchain
 
 class PChain {
 
-    internal val blockchain = mutableListOf<Block>()
+    companion object {
+        private const val MINE_DIFFICULTY = 4
+        private val BLOCKCHAIN = mutableListOf<Block>()
+
+    }
 
     fun isChainValid(): Boolean {
         var currentBlock: Block
         var previousBlock: Block
 
-        for (i in 1 until blockchain.size) {
-            currentBlock = blockchain[i]
-            previousBlock = blockchain[i - 1]
+        for (i in 1 until BLOCKCHAIN.size) {
+            currentBlock = BLOCKCHAIN[i]
+            previousBlock = BLOCKCHAIN[i - 1]
 
             if (currentBlock.hash != currentBlock.calcHash()) {
+                println("Hash Missmatch on $currentBlock")
                 return false
             }
 
             if (currentBlock.previousHash != previousBlock.hash) {
+                println("Invalid previous hash on $currentBlock")
+                return false
+            }
+
+            if (!currentBlock.hash.matches(("^[0]{$MINE_DIFFICULTY}.*").toRegex())) {
+                println("Invalid mining result on $currentBlock")
                 return false
             }
         }
         return true
     }
+
+    fun add(data: String) {
+        val previousHash = BLOCKCHAIN.getOrNull(BLOCKCHAIN.size - 1)?.hash ?: "0"
+        val newBlock = Block(previousHash, data)
+        BLOCKCHAIN.add(newBlock)
+        println("Mine block for $data")
+        newBlock.mineBlock(MINE_DIFFICULTY)
+    }
+
+    override fun toString(): String {
+        return "PChain: $BLOCKCHAIN"
+    }
+
+
 }
 
 fun main(args: Array<String>) {
 
     val pChain = PChain()
 
-    pChain.blockchain.add(Block("0", "I'm the first"))
-    pChain.blockchain.add(Block(pChain.blockchain[pChain.blockchain.size - 1].hash, "I'm the second"))
-    pChain.blockchain.add(Block(pChain.blockchain[pChain.blockchain.size - 1].hash, "I'm the third"))
-    pChain.blockchain.add(Block(pChain.blockchain[pChain.blockchain.size - 1].hash, "I'm the fourth"))
+    pChain.add("I'm the first")
+    pChain.add("I'm the second")
+    pChain.add("I'm the third")
+    pChain.add("I'm the fourth")
 
-    println(pChain.blockchain)
-    println(pChain.isChainValid())
+    println(pChain)
+    println("Valid Chain: ${pChain.isChainValid()}")
 }
